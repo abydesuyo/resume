7d7ab46ff8552e7b959b587f88ef5be42e6de526
abydesuyo <38089225+abydesuyo@users.noreply.github.com>
2021-03-07 16:55:10 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume/index.html/precache-manifest.a18780a267273f7ae15fbd1c9d3e4e0b.js"
+  "/resume/precache-manifest.6a75fca5ba54aa3b4ddb4928ace48b10.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html/index.html", {
+workbox.routing.registerNavigationRoute("/resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
