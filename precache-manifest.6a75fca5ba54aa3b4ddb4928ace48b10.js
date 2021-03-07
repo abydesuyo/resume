8702222864aaac631d6a0490ae8fa3e5ec8f74df
abydesuyo <38089225+abydesuyo@users.noreply.github.com>
2021-03-07 15:26:51 +0900

    Updates

---
+++ precache-manifest.6a75fca5ba54aa3b4ddb4928ace48b10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b401fa91bde8ee5228c1",
+    "url": "/resume/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "b401fa91bde8ee5228c1",
+    "url": "/resume/static/js/main.d9c9ed11.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "e79b61bdcb9c0a4f9205",
+    "url": "/resume/static/js/2.99696183.chunk.js"
+  },
+  {
+    "revision": "4ebd8b85338af009f6210c0571d5dd83",
+    "url": "/resume/index.html"
+  }
+];
